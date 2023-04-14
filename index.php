b976b750c9dfbe014070c96988449004d1357aff
NicholasConti <conti94@live.it>
2023-04-14 16:39:58 +0200

    array genere

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ class Movie
     public $anno;
     public $trama;
     public $linguaOriginale;
+    public $genere;
 
-    public function __construct($_title, int $_year, $_story, $_ogLang)
+    public function __construct($_title, int $_year, $_story, $_ogLang, array $_gen)
     {
         $this->titolo = $_title;
         $this->anno = $_year;
         $this->trama = $_story;
         $this->changeLanguage($_ogLang);
+        $this->genere = $_gen;
     }
 
     public function changeLanguage($lang)
@@ -30,7 +32,7 @@ class Movie
     }
 }
 
-$gatto = new Movie('Il gatto con gli stivali', 2023, 'lorem', 'it');
-$rick = new Movie('Rick e Morty', 2022, 'lorem ipsum ciao', 'en');
+$gatto = new Movie('Il gatto con gli stivali', 2023, 'lorem', 'it', ['Cartone animato', 'Fanatsy', 'Avventura']);
+$rick = new Movie('Rick e Morty', 2022, 'lorem ipsum ciao', 'en', ['Cartone animato', 'Commedia']);
 var_dump($gatto);
 var_dump($rick);
