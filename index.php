8db5edc92ca090d9b5b652c8f2706db02ffabe87
NicholasConti <conti94@live.it>
2023-04-14 15:52:36 +0200

    istanze

--- index.php
+++ index.php
@@ -5,4 +5,32 @@ class Movie
     public $anno;
     public $trama;
     public $linguaOriginale;
+
+    public function __construct($_title, int $_year, $_story, $_ogLang)
+    {
+        $this->titolo = $_title;
+        $this->anno = $_year;
+        $this->trama = $_story;
+        $this->changeLanguage($_ogLang);
+    }
+
+    public function changeLanguage($lang)
+    {
+        if (($lang === 'it') || ($lang === 'IT')) {
+            $lang = 'Italiano';
+        } elseif (($lang === 'en') || ($lang === 'EN')) {
+            $lang = 'Inglese';
+        } elseif (($lang === 'jp') || ($lang === 'JP')) {
+            $lang = 'Giapponese';
+        } elseif (($lang === 'kr') || ($lang === 'KR')) {
+            $lang = 'Koreano';
+        }
+
+        return $this->linguaOriginale = $lang;
+    }
 }
+
+$gatto = new Movie('Il gatto con gli stivali', 2023, 'lorem', 'it');
+$rick = new Movie('Rick e Morty', 2022, 'lorem ipsum ciao', 'en');
+var_dump($gatto);
+var_dump($rick);
